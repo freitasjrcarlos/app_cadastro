9748f69662263b5fa5825ad6c6472f7906ee24f7
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-04-12 17:03:14 -0300

    Add estrutura dos TextInput

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View, Image} from 'react-native';
 import firebase from 'firebase';
 import ImagePicker from 'react-native-image-picker';
 import RNFetchBlob from 'react-native-fetch-blob';
@@ -14,7 +14,12 @@ export default class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      formAvatar: null,
+      formNome: '',
+      formEmail: '',
+      formSenha: ''
+    };
 
     /* Conexão com Firebase */
     let config = {
@@ -38,7 +43,23 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> Teste </Text>
+        <View style={styles.cadastroArea}>
+          <Text> Cadastre um novo usuário </Text>
+
+          <View style={styles.form}>
+            <Image source={this.state.formAvatar} style={styles.formAvatar} />
+            <View style={styles.formInfo}>
+
+              <TextInput style={styles.input} placeholder="Nome" value={this.state.formNome} onChangeText={(formNome)=>this.setState({formNome})} />
+
+              <TextInput style={styles.input} placeholder="Email" value={this.state.formEmail} onChangeText={(formEmail)=>this.setState({formEmail})} />
+
+              <TextInput style={styles.input} secureTextEntry={true} placeholder="Senha" value={this.state.formSenha} onChangeText={(formSenha)=>this.setState({formSenha})} />
+
+            </View>
+          </View>
+
+        </View>
       </View>
     );
   }
@@ -49,7 +70,30 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: 20,
+  },
+  cadastroArea: {
+    height: 300,
+    backgroundColor: '#eeeeee',
+    margin: 10,
+    padding: 10,
+  },
+  form: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  formAvatar: {
+    width: 100,
+    height: 100
+  },
+  formInfo: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#000000',
+
   }
 });

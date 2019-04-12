4426fc536160f731cbb41a7fb81b40ad7b65804f
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-04-12 17:29:00 -0300

    Add FlatList para listagem dos dados

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, TextInput, View, Image, Button} from 'react-native';
+import {StyleSheet, Text, TextInput, View, Image, Button, FlatList} from 'react-native';
 import firebase from 'firebase';
 import ImagePicker from 'react-native-image-picker';
 import RNFetchBlob from 'react-native-fetch-blob';
@@ -18,7 +18,8 @@ export default class App extends Component {
       formAvatar: null,
       formNome: '',
       formEmail: '',
-      formSenha: ''
+      formSenha: '',
+      lista: [],
     };
 
     /* Conexão com Firebase */
@@ -50,25 +51,37 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+
         <View style={styles.cadastroArea}>
           <Text> Cadastre um novo usuário </Text>
-
           <View style={styles.form}>
             <Image source={this.state.formAvatar} style={styles.formAvatar} />
             <View style={styles.formInfo}>
-
               <TextInput style={styles.input} placeholder="Nome" value={this.state.formNome} onChangeText={(formNome)=>this.setState({formNome})} />
-
               <TextInput style={styles.input} placeholder="Email" value={this.state.formEmail} onChangeText={(formEmail)=>this.setState({formEmail})} />
-
               <TextInput style={styles.input} secureTextEntry={true} placeholder="Senha" value={this.state.formSenha} onChangeText={(formSenha)=>this.setState({formSenha})} />
-
             </View>
           </View>
-
           <Button title="Cadastrar" onPress={this.cadastrar} />
-
         </View> 
+
+        <View style={styles.listaArea}>
+          <FlatList
+            data={this.state.lista}
+            renderItem={(item)=> {
+              return(
+                <View style={styles.itemArea}>
+                  <Image source={item.avatar} style={styles.itemAvatar} />
+                  <View style={styles.itemInfo}>
+                    <Text>{item.nome}</Text>
+                    <Text>{item.email}</Text>
+                  </View>
+                </View>
+              );
+            }}
+          />
+        </View>
+
       </View> 
     );
   }
@@ -80,9 +93,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 20,
+    backgroundColor: '#ffffff'
   },
   cadastroArea: {
-    height: 200,
+    height: 240,
     backgroundColor: '#eeeeee',
     margin: 10,
     padding: 10,
@@ -93,7 +107,8 @@ const styles = StyleSheet.create({
   },
   formAvatar: {
     width: 100,
-    height: 100
+    height: 100,
+    backgroundColor: '#cccccc'
   },
   formInfo: {
     flex: 1,
@@ -103,6 +118,25 @@ const styles = StyleSheet.create({
     height: 40,
     borderWidth: 1,
     borderColor: '#000000',
-
+    margin: 5
+  },
+  listaArea: {
+    flex: 1,
+    backgroundColor: '#eeeeee',
+    margin: 10,
+  },
+  itemArea: {
+    height: 100,
+    flex: 1,
+    flexDirection: 'row',
+  },
+  itemAvatar: {
+    width: 80,
+    height: 80,
+    margin: 10,
+  },
+  itemInfo: {
+    flex: 1,
+    flexDirection: 'column',
   }
 });
